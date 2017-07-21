e73e98275220dd4d2f63c45dcfbe71ad3b6d645d
Administrator <Administrator@10.0.0.112>
2017-07-21 11:29:55 +0800

    2017-7-21

--- SpringMVC/src/aop/AopTest3.java
+++ SpringMVC/src/aop/AopTest3.java
@@ -13,10 +13,10 @@ import aop.service.Thinker;
  * 
  *         2016-6-14 上午09:54:54
  */
-public class AopTest2 {
+public class AopTest3 {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"aop/xml/spring_aop2.xml");
+				"aop/xml/spring_aop3.xml");
 		System.out.println("****************************************");
 		Thinker thinker=(Thinker) context.getBean("thinkerImpl");
 		thinker.thinkOfSomething("插入数据!");
